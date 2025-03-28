7f2bd2665d5b5d74170fb7517c5793eac32b3b04
Ann-Lore <annlore.schoonyans1@gmail.com>
2025-03-28 16:26:12 +0100

    ajout test privilèves user db

--- tp6/www/test_db.php
+++ tp6/www/test_db.php
@@ -1,10 +1,10 @@
 <?php
-$host = "db";
-$user = "root";
-$pass = "mypass";
-$dbname = "woodytoys";
+$dbname = getenv('MARIADB_DATABASE');
+$dbhost = getenv('MARIADB_HOST');
+$dbuser = getenv('MARIADB_USER');
+$dbpass = getenv('MARIADB_PASSWORD');
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 if ($conn->connect_error) {
     die("Échec de connexion : " . $conn->connect_error);
@@ -12,5 +12,13 @@ if ($conn->connect_error) {
     echo "Connexion réussie à la base de données !";
 }
 
+$query = "INSERT INTO products (id, product_name, product_price) VALUES (999, 'Test', 0.99)";
+if (mysqli_query($conn, $query)) {
+    echo "Erreur : l'insertion est possible, ce qui est un problème !";
+} else {
+    echo "OK : l'insertion est bien bloquée.";
+}
+
 $conn->close();
 ?>
+
